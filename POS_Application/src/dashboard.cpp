1afcbe91e0179b5ba5a5e5d9b427c0baa78188dc
Aaron Sanchez <aaroncz032@gmail.com>
2025-01-08 11:53:33 -0600

    chore: Deletion of unnecesary code lines.

--- POS_Application/src/dashboard.cpp
+++ POS_Application/src/dashboard.cpp
@@ -2,21 +2,12 @@
 
 #include "dashboard.h"
 #include "ui_dashboard.h"
-#include "product.h"
 #include "backupcontroller.h"
 
 Dashboard::Dashboard(QWidget *parent) :
   QWidget(parent),
   ui(new Ui::DashboardPage) {
   ui->setupUi(this);
-  std::cout << "creando un plato" << std::endl;
-  std::vector<SupplyItem> ingredients;
-  ingredients.push_back(SupplyItem(std::string("arroz"), 300));
-  ingredients.push_back(SupplyItem(std::string("carne"), 300));
-  ingredients.push_back(SupplyItem(std::string("ensalada"), 300));
-  std::string dishName("Casado");
-  Product food(2, dishName, ingredients, 3500);
-  std::cout << "plato creado: " << food << std::endl;
   
   BackUpController backupMemory = BackUpController::getInstance();
   backupMemory.start();
