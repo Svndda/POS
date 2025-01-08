ae06aebaaf36dd603a4d71d6d1e4dd4955cc092f
Aaron Sanchez <aaroncz032@gmail.com>
2025-01-08 11:56:13 -0600

    feat/refactor: Added registeredProducts and reduction of redundant code.
    - Added registeredProducts attribute that contains all the registered products.
    - Deletion of label updation for a modular function.

--- POS_Application/src/inventory.h
+++ POS_Application/src/inventory.h
@@ -20,27 +20,33 @@ class Inventory : public QWidget
 public:
   explicit Inventory(QWidget *parent = nullptr);
   ~Inventory();
-
+  
+  // Private class attributes.
 private:
   Ui::InventoryPage *ui;
   // Object private data structures that contains the POS registered drinks.
   std::map<std::string, std::vector<Product>> registeredDrinks;
   // Object private data structures that contains the POS registered dishes.
   std::map<std::string, std::vector<Product>> registeredDishes;
+  // Object private data structures that contains the POS registered products
+  //  combined.
+  std::vector<std::pair<std::string, Product>> registeredProducts;
+  size_t productPageIndex = 0;
+  
+  // Private class functions.
+private:
+  void registerProductType(
+      std::vector<std::pair<std::string, Product>>& registeredProducts
+      , const std::map<std::string, std::vector<Product>>& productTypeRegister);
+  QString formatProductIngredients(const std::vector<SupplyItem>& ingredients);
   void updateProductsInformation(
       std::vector<std::pair<std::string, Product>> visibleProducts);
-  void updateProductsID(
-      std::vector<std::pair<std::string, Product>> visibleProducts,
-      size_t index);
-  void updateProductsName(
-      std::vector<std::pair<std::string, Product>> visibleProducts,
-      size_t index);
-  void updateProductsCategories(
-      std::vector<std::pair<std::string, Product>> visibleProducts,
-      size_t index);
-  void updateProductsPrices(
-      std::vector<std::pair<std::string, Product>> visibleProducts,
-      size_t index);
+  void updateProductLabel(size_t labelIt,
+      const QString& labelSuffix, const QString& value);
+  
+private slots:
+  void on_nextProductPage_button_clicked();
+  void on_previousProductPage_button_clicked();
 };
 
 #endif // INVENTORY_H
