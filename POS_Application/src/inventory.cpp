d10da7a212c295dc6ef38464243e8713a7bc85a3
Aaron Sanchez <aaroncz032@gmail.com>
2025-01-08 11:56:51 -0600

    feat/refactor: Added registeredProducts and reduction of redundant code.
    - Added registeredProducts attribute that contains all the registered products.
    - Deletion of label updation for a modular function.

--- POS_Application/src/inventory.cpp
+++ POS_Application/src/inventory.cpp
@@ -10,182 +10,199 @@
 QLabel* cloneLabel(QLabel* original) {
   QLabel* newLabel = new QLabel(original->parentWidget());
   
-  // Copiar texto y propiedades importantes
+  // Copy the important attributes.
   newLabel->setText(original->text());
   newLabel->setAlignment(original->alignment());
   newLabel->setStyleSheet(original->styleSheet());
   newLabel->setFont(original->font());
   newLabel->setPixmap(original->pixmap(Qt::ReturnByValue)); // Si usa pixmap
   
-  // Otros ajustes si es necesario
+  // Other settings if needed.
   newLabel->setSizePolicy(original->sizePolicy());
   newLabel->setGeometry(original->geometry());
   
   return newLabel;
 }
 
+QString Inventory::formatProductIngredients(
+    const std::vector<SupplyItem>& ingredients) {
+  QString formattedProductIngredients = "";
+  
+  for (auto ingredient : ingredients) {
+    QString ingredientName(ingredient.getName().data());
+    QString ingredientQuantity = QString::number(ingredient.getQuantity());
+    formattedProductIngredients = QString(formattedProductIngredients
+      + " " +ingredientName.trimmed() + " : " + ingredientQuantity);
+  }
+  
+  return formattedProductIngredients;
+}
+
 Inventory::Inventory(QWidget *parent)
     : QWidget(parent)
     , ui(new Ui::InventoryPage)
     , registeredDrinks(std::map<std::string, std::vector<Product>>())
-    , registeredDishes(std::map<std::string, std::vector<Product>>()){
+    , registeredDishes(std::map<std::string, std::vector<Product>>())
+    , registeredProducts(std::vector<std::pair<std::string, Product>>())
+    , productPageIndex(0) {
   ui->setupUi(this);
+  // Obtain the instance of the backupController.
   BackUpController backupControllerInstance = BackUpController::getInstance();
+  // Start the backupController.
   backupControllerInstance.start();
+  
+  
+  // Gets the registered products.
   this->registeredDrinks = backupControllerInstance.getRegisteredDrinks();
   this->registeredDishes = backupControllerInstance.getRegisteredDishes();
   
-  std::vector<std::pair<std::string, Product>> visibleProducts;
+  // Creates a vector for the products to be displayed in screen.
+  std::vector<std::pair<std::string, Product>> mergedProducts;
   
-  // Iterar sobre las categorías y productos
-  for (const auto& [category, products] : this->registeredDishes) {
-    // Breaks if theres already 9 product names recolected.
-    if (visibleProducts.size() == 9) break;
-    for (const auto& product : products) {
-      // Breaks if theres already 9 product names recolected.
-      if (visibleProducts.size() == 9) break;
-      visibleProducts.push_back(std::pair<std::string, Product>(category,
-          product));
-    }
-  }
+  this->registerProductType(mergedProducts, this->registeredDrinks);
+  this->registerProductType(mergedProducts, this->registeredDishes);
+  this->registeredProducts = mergedProducts;
   std::cout << "Registered Dishes's Category: " << this->registeredDishes.size()
       << std::endl;
   std::cout << "Registered Drink's Category: " << this->registeredDrinks.size()
             << std::endl;
   
-  this->updateProductsInformation(visibleProducts);
+  // Updates for the first time the diaply to show the first index page.
+  this->updateProductsInformation(this->registeredProducts);
+  
+  // Connect the slot function to the next page button.
+  connect(this->ui->nextProductPage_button,
+          &QPushButton::clicked, this, []() {});
+  
+  // Connect the slot function to the previous page button.
+  connect(this->ui->previousProductPage_button,
+      &QPushButton::clicked, this, []() {});
+}
+    
+void Inventory::on_nextProductPage_button_clicked() {
+  // Calculates the indexes of the first and last product to be fopr the
+  // nect page displayed.
+  size_t productPageIt = (this->productPageIndex + 1) * 9;
+  size_t productPageIt2 = productPageIt + 9;
+  // Checks if the size of the registered products vector is lower than the
+  // indexes of the next page.
+  if (this->registeredProducts.size() >= productPageIt
+      && this->registeredProducts.size() <= productPageIt2) {
+    ++this->productPageIndex;
+    // Updates the product information in the display to show the next product
+    // page.
+    this->updateProductsInformation(this->registeredProducts); 
+  }
+  qDebug() << "Boton de avance: " << this->productPageIndex;
+}
+
+void Inventory::on_previousProductPage_button_clicked() {
+  // Checks that the actual page is not the first one.
+  if (this->productPageIndex > 0) {
+    // Decrements the page index.
+    --this->productPageIndex;
+    // Updates the product information in the display to show the previous
+    // product page.
+    this->updateProductsInformation(this->registeredProducts);    
+  }
+  std::cout << "Boton de retroceso: " << this->productPageIndex << std::endl;
+}
+
+void Inventory::registerProductType(
+    std::vector<std::pair<std::string, Product>>& registeredProducts
+    , const std::map<std::string, std::vector<Product>>& productTypeRegister) {
+  // Transverse all the product type categories and their products.
+  for (const auto& [category, products] : productTypeRegister) {
+    // Transverse all the products of the category and added them to the vector.
+    for (const auto& product : products) {
+      // Emplace the product in the the product register.
+      registeredProducts.emplace_back(category, product);
+    }
+  }
 }
 
 void Inventory::updateProductsInformation(
     std::vector<std::pair<std::string, Product>> visibleProducts) {
+  // Calculates an offset to calculate the indexes of the products to display on
+  // the screen.
+  const size_t offset = 0 + (9 * this->productPageIndex);
+  // Calculates the last index of the actual product information page.
+  size_t finalOffset = offset + 9;
+  // Initiazates the label index iterator.
+  size_t labelIt = 0;
   // Tranverse the different rows of products information.
-  for (size_t index = 0; index < 9; index++) {
+  for (size_t index = offset; index < finalOffset; index++) {
+    // PlaceHolder labels text.
+    QString productID("------");
+    QString productName("------");
+    QString productCategory("------");
+    QString productIngredients("------");
+    QString productPrice("------");
+    
+    // If theres remaining products information to be displayed.
+    if (index < visibleProducts.size()) {
+      // Extracts all the product information to display it on the screen.
+      productID = 
+          std::to_string(visibleProducts[index].second.getID()).data();
+      productName = visibleProducts[index].second.getName().data();
+      productCategory = visibleProducts[index].first.data();
+      productPrice = QString::number(visibleProducts[index].second.getPrice(),
+          'f', 2);
+      productIngredients = this->formatProductIngredients(
+          visibleProducts[index].second.getIngredients());
+    }
     // Updates the the different labels for each product information that is
     // displayed in the UI.
-    this->updateProductsID(visibleProducts, index);
-    this->updateProductsName(visibleProducts, index);
-    this->updateProductsCategories(visibleProducts, index);
-    this->updateProductsPrices(visibleProducts, index);
+    
+    // Updates the product text contained in the productCode of the matching
+    // label.    
+    this->updateProductLabel(labelIt, "productCode", productID);
+    // Updates the product text contained in the productName of the matching
+    // label.    
+    this->updateProductLabel(labelIt, "productName", productName);
+    // Updates the product text contained in the productCategory of the matching
+    // label.    
+    this->updateProductLabel(labelIt, "productCategory", productCategory);
+    // Updates the product text contained in the productIngredients of the
+    // matching label.    
+    this->updateProductLabel(labelIt, "productIngredients", productIngredients);
+    // Updates the product text contained in the productPrice of the matching
+    // label.    
+    this->updateProductLabel(labelIt, "productPrice", productPrice);    
+    // Increse the label iterator.
+    ++labelIt;
   }
+  
+  // Checks if the final offset is lower than the visible items.
+  if (finalOffset > visibleProducts.size()) {
+    finalOffset = visibleProducts.size();
+  }
+  
+  // Generates the page label text tha indicates the indexes of the products
+  // displayed in the actual page.
+  QString pageLabelText = QString("Mostrando productos %1 hasta %2 de %3"
+      " productos").arg(offset).arg(finalOffset).arg(visibleProducts.size());
+  this->ui->pageProductsNumber_label->setText(pageLabelText);
 }
 
-void Inventory::updateProductsID(
-    std::vector<std::pair<std::string, Product>> visibleProducts,
-    size_t index) {
-  // Label row number.
-  size_t labelNumber = index;
-  // Product's name that are going to be displayed.
-  QString productID;
-  // Checks if the index is lower than the next visible products to display.
-  if (index < visibleProducts.size()) {
-    productID = QString(
-        std::to_string(visibleProducts[index].second.getID()).data());
-  } else {
-    // Empty product placeholder
-    productID = QString("--------");
-  }
+void Inventory::updateProductLabel(size_t labelIt,
+    const QString& labelSuffix, const QString& value) {
   
-  // Appends the labelsubfix with the label index.
-  QString labelName = QString("productCode_label_%1").arg(labelNumber);
-  // Search a label in the ui with that name.
+  // Generates the label name based on the suffix and label index.
+  QString labelName = QString("%1_label_%2").arg(labelSuffix).arg(labelIt);
   QLabel* label = this->findChild<QLabel*>(labelName);
   
-  // QLabel* newlabel = cloneLabel(label);
-  // this->ui->productNameVerticalLayout->addWidget(newlabel);
-  
+  // Checks if catched a label with that name.
   if (label != nullptr) {
-    // Sets the product's name as the label text.
-    label->setText(productID.trimmed());
+    // Updates the label text with the given text value.
+    label->setText(value.trimmed());
     label->setAlignment(Qt::AlignCenter);
   } else {
-    qDebug() << "No se pudo encontrar el puntero del label de ID indicado.";
+    qDebug() << "No se pudo encontrar el puntero del label con sufijo: "
+             << labelSuffix;
   }
 }
 
-void Inventory::updateProductsName(
-    std::vector<std::pair<std::string, Product>> visibleProducts,
-    size_t index) {
-  // Label row number.
-  size_t labelNumber = index;
-  // Product's name that are going to be displayed.
-  QString productName;
-  // Checks if the index is lower than the next visible products to display.
-  if (index < visibleProducts.size()) {
-    productName = QString(visibleProducts[index].second.getName().data());
-  } else {
-    // Empty product placeholder
-    productName = QString("--------");
-  }
-  
-  // Appends the labelsubfix with the label index.
-  QString labelName = QString("productName_label_%1").arg(labelNumber);
-  // Search a label in the ui with that name.
-  QLabel* label = this->findChild<QLabel*>(labelName);
-  
-  // QLabel* newlabel = cloneLabel(label);
-  // this->ui->productNameVerticalLayout->addWidget(newlabel);
-  
-  if (label != nullptr) {
-    // Sets the product's name as the label text.
-    label->setText(productName.trimmed());
-    label->setAlignment(Qt::AlignCenter);
-  } else {
-    qDebug() << "No se pudo encontrar el puntero del label de nombre indicado.";
-  }
-}
-
-void Inventory::updateProductsCategories(
-    std::vector<std::pair<std::string, Product>> visibleProducts,
-    size_t index) {
-  // Label row number.
-  size_t labelNumber = index;
-  // Product's category that are going to be displayed.
-  QString productCategory;
-  if (index < visibleProducts.size()) {
-    productCategory = QString((visibleProducts[index].first.data()));
-  } else {
-    // Empty product placeholder
-    productCategory = QString("--------");
-  }
-  // Appends the labelsubfix with the label index.  
-  QString labelName = QString("productCategory_label_%1").arg(labelNumber);
-  // Search a label in the ui with that name.  
-  QLabel* label = this->findChild<QLabel*>(labelName);
-  
-  if (label != nullptr) {
-    // Sets the product's category as the label text.
-    label->setText(productCategory);
-  } else {
-    qDebug() << "No se pudo encontrar el puntero del label de categoria"
-        " indicado.";
-  }
-}
-
-void Inventory::updateProductsPrices(
-    std::vector<std::pair<std::string, Product>> visibleProducts,
-    size_t index) {
-  // Label row number.
-  size_t labelNumber = index;
-  // Product's price that are going to be displayed.
-  QString productPrice;
-  if (index < visibleProducts.size()) {
-    productPrice = QString((visibleProducts[index].first.data()));
-  } else {
-    // Empty product placeholder
-    productPrice = QString("--------");
-  }
-  // Appends the labelsubfix with the label index.  
-  QString labelName = QString("productPrice_label_%1").arg(labelNumber);
-  // Search a label in the ui with that name.  
-  QLabel* label = this->findChild<QLabel*>(labelName);
-  
-  if (label != nullptr) {
-    // Sets the product's price as the label text.
-    label->setText(productPrice);
-  } else {
-    qDebug() << "No se pudo encontrar el puntero del label de precio indicado.";
-  }
-}
 
 Inventory::~Inventory() {
   delete ui;
