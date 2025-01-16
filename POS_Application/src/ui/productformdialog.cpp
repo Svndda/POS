8bb4e4494d18ed968b7f1b89b776225c3d756c30
Aaron Sanchez <aaroncz032@gmail.com>
2025-01-16 07:57:37 -0600

    feat/refactor: The object now can edit an existing product and some memory improvements.

--- POS_Application/src/ui/productformdialog.cpp
+++ POS_Application/src/ui/productformdialog.cpp
@@ -1,70 +1,141 @@
 #include <sstream>
 #include <string>
 
+#include <QMessageBox>
 #include "productformdialog.h"
 #include "ui_productformdialog.h"
 
 ProductFormDialog::ProductFormDialog(QWidget *parent
-    , std::map<std::string, std::vector<Product>> drinks
-    , std::map<std::string, std::vector<Product>> dishes)
+    , std::map<std::string, std::vector<Product>>& products
+    , Product product
+    , QString category)
     : QDialog(parent)
-    , registeredDrinks(drinks)
-    , registeredDishes(dishes)
+    , registeredProducts(products)
+    , createdProduct(product)
+    , productCategory(category)
     , ui(new Ui::ProductFormDialog) {
   ui->setupUi(this);
   std::vector<QString> productCategories;
   
-  for (const auto& category : this->registeredDrinks) {
+  for (const auto& category : this->registeredProducts) {
     productCategories.push_back(category.first.data());
   }
   
-  for (const auto& category : this->registeredDishes) {
-    productCategories.push_back(category.first.data());
-  }
   
   for (const auto& category : productCategories) {
     this->ui->productCategory_comboBox->addItem(category);
   }
   
-  this->createdProduct = Product();
+  // Checks if product information were provided, if so, displays it.
+  if (!(this->createdProduct == Product())) {
+    this->setProductInfo(this->createdProduct, this->productCategory);
+  }
   
+  // Connects the slot of the accept button of the QDialog.
   connect(ui->acceptProduct_button
           , &QPushButton::clicked
           , this
           , &ProductFormDialog::on_acceptProduct_button_clicked);
+  // Connects the slot of the cancel button of the QDialog.  
   connect(ui->cancelProduct_button
           , &QPushButton::clicked
           , this
           , &ProductFormDialog::on_cancelProduct_button_clicked);
 }
 
+QString ProductFormDialog::formatProductIngredients(
+    const std::vector<SupplyItem>& ingredients) {
+  // String that will contain the ingredients information formated
+  //  to be displayed.
+  QString formattedProductIngredients = "";
+  
+  // Iterates through all the ingredients contained in the vector.
+  for (auto it = ingredients.begin(); it != ingredients.end(); ++it) {
+    // Saves the ingredient's information.
+    QString ingredientName(it->getName().data());
+    QString ingredientQuantity = QString::number(it->getQuantity());
+    // Stablish a separator between ingredients.
+    if (it == ingredients.end() - 1) {
+      formattedProductIngredients = QString(formattedProductIngredients
+          + ingredientName.trimmed() + " " + ingredientQuantity);
+    } else {
+      formattedProductIngredients = QString(formattedProductIngredients
+          + ingredientName.trimmed() + " " + ingredientQuantity + ", ");
+    }
+  }
+  // Returns a string containing all the ingredients information formated to be
+  // displayed.
+  return formattedProductIngredients;
+}
+
 void ProductFormDialog::on_cancelProduct_button_clicked() {
+  // Stablish that the QDialog has finished incorrectly.
   this->reject();
 }
 
 void ProductFormDialog::on_acceptProduct_button_clicked() {
+  // Product's information provided by the user.
   QString productCategory = this->ui->productCategory_comboBox->currentText();
   QString productName = this->ui->productName_lineEdit->text();
   QString productIngredients = this->ui->productIngredients_lineEdit->text();
   double productPrice = this->ui->productPrice_doubleSpinBox->value();
   
+  QRegularExpression regex(R"(^\s*\p{L}+\s+\d+(\s*,\s*\p{L}+\s+\d+)*\s*$)");
+  // Checks if product name were provided.
   if (!productName.isEmpty()) {
-    std::vector<SupplyItem> ingredients;
-    std::istringstream buffer(productIngredients.toStdString());
-    std::string item;
-    
-    while(std::getline(buffer, item, ',')) {
-      std::istringstream itemBuffer(item);
-      std::string name = "";
-      uint64_t quantity = 0;
-      itemBuffer >> name >> quantity;
-      ingredients.emplace_back(name, quantity);
+    // Checks that the product ingredients match the stablished format.
+    if (regex.match(productIngredients).hasMatch()) {
+      // vector of product ingredients.
+      std::vector<SupplyItem> ingredients;
+      // buffer containing all the ingredients information.
+      std::istringstream buffer(productIngredients.toStdString());
+      // String to almacenate each ingredient information.
+      std::string item;
+      
+      // Separates the different ingredients data from the string.
+      while(std::getline(buffer, item, ',')) {
+        // Stream for each ingredient.
+        std::istringstream itemBuffer(item);
+        // Ingredients data.
+        std::string name = "";
+        uint64_t quantity = 0;
+        // Reads the ingredients data from the buffer.
+        itemBuffer >> name >> quantity;
+        // Emplace a new ingredients to the product's vector ingredients.
+        ingredients.emplace_back(name, quantity);
+      }
+      // Store a new product into with information given by the user.
+      this->createdProduct = Product(1, productName.toStdString()
+          , ingredients, productPrice);
+      // Stablish that the Qdialog has finished correctly.
+      this->accept();
+    } else {
+      // Creates a message box to indicate the user that the format in the
+      // product ingredients is wrong.
+      QMessageBox::warning(this, "Error en formato de ingredientes",
+          "Por favor, escriba los ingredientes del producto en el formato"
+          " solicitado");
     }
-    qDebug() << productName << productCategory << productPrice << productIngredients;
-    this->createdProduct = Product(1,
-        productName.toStdString(), ingredients, productPrice);
-    this->accept();
   }
+}
+
+void ProductFormDialog::setProductInfo(
+    Product& productToEdit, QString productCategory) {
+  // Index value for the category provided to the QDialog.
+  size_t categoryIndex
+      = this->ui->productCategory_comboBox->findText(productCategory
+      , Qt::MatchContains);
+  // Sets the category combo box to the index of the product's category.
+  this->ui->productCategory_comboBox->setCurrentIndex(categoryIndex);
+  // Sets the text of the lineEdit of the product name.
+  this->ui->productName_lineEdit->setText(productToEdit.getName().data());
+  // Creates a string containing all the ingredients information.
+  QString productIngredients = this->formatProductIngredients(
+      productToEdit.getIngredients());
+  // Sets the text of the lineEdit of the product ingredients.
+  this->ui->productIngredients_lineEdit->setText(productIngredients);
+  // Sets the value of the double spin box of the product price.
+  this->ui->productPrice_doubleSpinBox->setValue(productToEdit.getPrice());
 }
 
 QString ProductFormDialog::getProductCategory() {
--- POS_Application/src/ui/productformdialog.h
+++ POS_Application/src/ui/productformdialog.h
@@ -10,29 +10,100 @@ namespace Ui {
 class ProductFormDialog;
 }
 
+/**
+ * @class ProductFormDialog
+ * @brief A dialog for displaying and editing product details.
+ * 
+ * This dialog allows the user to enter or edit the details of a product, such as 
+ * its name, ingredients, price, and category. The dialog also validates the input
+ * before creating or updating the product.
+ */
 class ProductFormDialog : public QDialog {
   Q_OBJECT
   
 private:
+  // Pointer to the ui object.
   Ui::ProductFormDialog *ui;
-  std::map<std::string, std::vector<Product>> registeredDrinks;
-  std::map<std::string, std::vector<Product>> registeredDishes;
-  Product createdProduct = Product(); 
+  // Map that contains all the registered drinks based on their category.
+  std::map<std::string, std::vector<Product>>& registeredProducts;
+  // Object that contains all the product information.
+  Product createdProduct = Product();
+  // String that contains the product's category.
+  QString productCategory = QString();
 
 public:
-  explicit ProductFormDialog(QWidget *parent = nullptr,
-      std::map<std::string, std::vector<Product>> drinks
-          = std::map<std::string, std::vector<Product>>(),
-      std::map<std::string, std::vector<Product>> dishes
-          = std::map<std::string, std::vector<Product>>());
+  /**
+   * @brief Constructor for the ProductFormDialog class.
+   * 
+   * Initializes the dialog, populates the product category combo box with available categories 
+   * from drinks and dishes, and sets up connections for the accept and cancel buttons.
+   * 
+   * @param parent Parent widget for the dialog.
+   * @param drinks A map of drinks categories and corresponding products.
+   * @param dishes A map of dishes categories and corresponding products.
+   * @param product A Product object containing information to pre-fill the dialog fields.
+   * @param category The product category to be selected initially in the combo box.
+   */
+  explicit ProductFormDialog(QWidget *parent
+      , std::map<std::string, std::vector<Product>>& products
+      , Product productToCreate
+      , QString productCategory);
+  
+  /**
+   * @brief Destructor for the ProductFormDialog class.
+   * 
+   * Cleans up the UI resources when the dialog is destroyed.
+   */
   ~ProductFormDialog();
   
+  /**
+   * @brief Formats a list of product ingredients into a string.
+   * 
+   * Iterates through a vector of SupplyItem objects and generates a formatted string 
+   * suitable for display in the product form.
+   * 
+   * @param ingredients A vector of SupplyItem objects to be formatted.
+   * @return A QString containing the formatted product ingredients.
+   */
+  QString formatProductIngredients(const std::vector<SupplyItem>& ingredients);
+  
+  /**
+   * @brief Retrieves the created or edited product.
+   * 
+   * @return A Product object containing the information entered or edited by the user.
+   */
   Product getProduct();
   
-  QString getProductCategory();  
+  /**
+   * @brief Retrieves the created or edited product.
+   * 
+   * @return A Product object containing the information entered or edited by the user.
+   */
+  QString getProductCategory();
   
+  /**
+   * @brief Sets the product information in the dialog fields.
+   * 
+   * Populates the product category, name, ingredients, and price into the respective UI elements.
+   * 
+   * @param productToEdit The product whose information will be displayed.
+   * @param productCategory The product category to be set in the combo box.
+   */
+  void setProductInfo(Product& productToEdit, QString productCategory);
 private slots:
+  /**
+   * @brief Slot connected to the accept button. Validates user input and saves the product information.
+   * 
+   * Validates the product name and ingredients entered by the user, ensuring that ingredients match the expected format.
+   * If valid, it creates a new product and accepts the dialog. Otherwise, it displays a warning message box.
+   */
   void on_acceptProduct_button_clicked();
+  
+  /**
+   * @brief Slot connected to the cancel button. Closes the dialog without saving.
+   * 
+   * Rejects the dialog, signaling that the user canceled the operation.
+   */
   void on_cancelProduct_button_clicked();
 };
 
