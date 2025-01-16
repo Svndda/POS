f9d9ffdf0a2a4c7f187609d3ee90e02d291abd1a
Aaron Sanchez <aaroncz032@gmail.com>
2025-01-16 08:10:04 -0600

    feat/refactor: Addition of model functionalities.
    - Added get functions that calculates or format most of the necessary data for the views.
    - Merge of the products in a single map.
    - Added a plain vector version of the products map to be a easier way to some information requested by the views.

--- POS_Application/src/model/posmodel.cpp
+++ POS_Application/src/model/posmodel.cpp
@@ -1,69 +1,166 @@
 #include <vector>
+#include <limits>
+
 #include <QDebug>
 
 #include "posmodel.h"
 #include "backupmodule.h"
 
-#define DRINKS_BACKUP_FILE "C:\\Users\\aaron\\Dev\\Repositories" \
-"\\POS\\POS_Application\\backup\\products\\Bebidas.txt"
-
-#define DISHES_BACKUP_FILE "C:\\Users\\aaron\\Dev\\Repositories" \
-"\\POS\\POS_Application\\backup\\products\\Comidas.txt"
-
-#define PRIME_BACKUP_FILE ""
+const size_t POS_Model::INSERT = 100;
+const size_t POS_Model::UPDATE = 200;
+const size_t POS_Model::DELETE = 0;
 
 POS_Model::POS_Model(BackupModule& module)
     : backupModule(module) {
 }
 
 POS_Model& POS_Model::getInstance() {
-  // Loads the data contained in the backup program files.
-  std::vector<std::string> backUpFiles = {
-      DRINKS_BACKUP_FILE,
-      DISHES_BACKUP_FILE,
-      ""};
-  static POS_Model instance(BackupModule::getInstance(backUpFiles));
+  // Creates an static instance of the POS MODEL.
+  static POS_Model instance(BackupModule::getInstance());
   return instance;
 }
 
 void POS_Model::start() {
   // Reads and store the backup to the program memory to use them in the
   // program execution.
-  this->Drinks = this->backupModule.readDrinksBackup();
-  this->Dishes = this->backupModule.readDishesBackup();
-  
+  this->products = this->backupModule.getProductsBackup();
+  this->productsVector.clear();
+  this->extractProducts(this->productsVector, this->products);
   // Changes the started value to true.
   this->started = true;
 }
 
-std::vector<std::pair<std::string, Product>> POS_Model::registeredProducts() {
-  std::vector<std::pair<std::string, Product>> mergedProducts;
-  this->extractProducts(mergedProducts, this->Drinks);
-  this->extractProducts(mergedProducts, this->Dishes);
-  // qDebug() << "products vector size: " << mergedProducts.size();
-  return mergedProducts;
-};
-
-void POS_Model::updateProductRegisters(const std::string productCategory,
-    const Product& product) {
-  
-  if (this->insertOnRegister(productCategory, product, this->Drinks)) {
-    qDebug() << "producto nuevo de bebidas";
-    this->backupModule.writeDrinksBackUp(this->Drinks);
-  } else if (this->insertOnRegister(productCategory, product, this->Dishes)) {
-    qDebug() << "producto nuevo de comidas";
-    this->backupModule.writeDishesBackUp(this->Dishes);
+Product& POS_Model::findProduct(const std::string& productName) {
+  for (auto& [category, products] : this->products) {
+    // Iterar sobre los productos en la categoría actual
+    for (auto& product : products) {
+      // Comparar el nombre del producto con el buscado
+      if (product.getName() == productName) {
+        return product; // Platillo encontrado
+      }
+    }
   }
+  throw std::runtime_error("Product not found: " + productName);  
+}
+
+void POS_Model::addProduct(const std::string& category
+    , const Product& product) {
+  if (insertOnRegister(category, product, this->products)) {
+    qDebug() << "Producto anadido correctamente";
+    backupModule.writeRegistersBackUp(this->products);
+  }
+}
+
+void POS_Model::removeProduct(const std::string& category
+    , const Product& product) {
+  if (eraseOnRegister(category, product, this->products)) {
+    qDebug() << "producto elimnado correctamente";
+    backupModule.writeRegistersBackUp(this->products);
+  }
+}
+
+void POS_Model::editProduct(const std::string& oldCategory
+    , const Product& oldProduct, const std::string& newCategory
+    , const Product& newProduct) {
+  if (eraseOnRegister(oldCategory, oldProduct, this->products)) {
+    insertOnRegister(newCategory, newProduct, this->products);
+    backupModule.writeRegistersBackUp(this->products);
+  }
+}
+
+std::vector<std::string> POS_Model::getCategoriesForPage(size_t pageIndex) {
+  std::vector<std::string> registeredCategories;
+  size_t startIdx = pageIndex * 9;
+  size_t endIdx = startIdx + 9;
+  
+  endIdx = std::min(endIdx, this->products.size());
+  
+  size_t currentIdx = 0;
+  
+  for (const auto& [category, product] : this->products) {
+    if (currentIdx >= startIdx && currentIdx < endIdx) {
+      registeredCategories.push_back(category);
+    }
+    ++currentIdx;
+    if (currentIdx >= endIdx) {
+      break;
+    }
+  }
+  
+  return registeredCategories;
+}
+
+size_t POS_Model::getSizeOfCategory(std::string category) {
+  size_t no_value = std::numeric_limits<size_t>::max();
+  for (const auto& element : this->products) {
+    if (element.first == category) {
+      return element.second.size();
+    }
+  }
+  return no_value;
+}
+
+std::vector<std::pair<std::string, Product>> POS_Model::getProductsForPage(
+    size_t pageIndex) {
+  size_t startIdx = pageIndex * 9;
+  size_t endIdx = std::min(startIdx + 9, this->productsVector.size());
+  
+  std::vector<std::pair<std::string, Product>> pageProducts;
+  for (size_t i = startIdx; i < endIdx; ++i) {
+    pageProducts.push_back(this->productsVector[i]);
+  }
+  return pageProducts;
 }
 
 bool POS_Model::insertOnRegister(const std::string productCategory
     , const Product& product
     , std::map<std::string, std::vector<Product>>& productTypeRegister) {
+  // Transverse all the registered product categories.
   for (auto& category : productTypeRegister) {
+    // Checks if the category matches.
     if (category.first == productCategory) {
-      qDebug() << "Categoria coincidente: " << category.first;
+      // Transverse all the registered products in the category.
+      for (auto& registeredProduct : category.second) {
+        // check if there's a registered product in the category.
+        if (registeredProduct == product) {
+          qDebug() << "Ya existe un elemento igual: "
+              << registeredProduct.getName() << "," << product.getName();
+          return false;
+        }
+      }
+      qDebug() << "Categoria anadiendo producto a la categoria: "
+               << category.first;
+      // Adds the created product into the vector of registered products.
       category.second.emplace_back(product);
+      this->productsVector.clear();      
+      this->extractProducts(this->productsVector, this->products);      
       return true;
+    }
+  }
+  return false;
+}
+
+bool POS_Model::eraseOnRegister(const std::string productCategory
+    , const Product& product
+    , std::map<std::string, std::vector<Product>>& productTypeRegister) {
+  // Transverse all the registered product categories.
+  for (auto& category : productTypeRegister) {
+    // Checks if the category matches.
+    qDebug() << category.first << ":" << productCategory;
+    if (category.first == productCategory) {
+      // Transverse all the registered products in the category.
+      for (size_t registerIt = 0; registerIt < category.second.size();
+           ++registerIt) {
+        // Check if there's a registered product in the category that matches
+        // the product to delete.
+        std::cout << category.second[registerIt] <<  ":" << product;   
+        if (category.second[registerIt] == product) {
+          category.second.erase(category.second.begin() + registerIt);
+          this->productsVector.clear();
+          this->extractProducts(this->productsVector, this->products);
+          return true;
+        }
+      }
     }
   }
   return false;
@@ -82,12 +179,24 @@ void POS_Model::extractProducts(
   }
 }
 
+QString POS_Model::formatProductIngredients(
+    const std::vector<SupplyItem>& ingredients) {
+  QString formattedProductIngredients = "";
+  
+  for (auto ingredient : ingredients) {
+    QString ingredientName(ingredient.getName().data());
+    QString ingredientQuantity = QString::number(ingredient.getQuantity());
+    formattedProductIngredients = QString(formattedProductIngredients
+        + " " +ingredientName.trimmed() + " : " + ingredientQuantity);
+  }
+  
+  return formattedProductIngredients;
+}
+
 void POS_Model::shutdown() {
   // Writes out the registers of the dishes and drinks back to the backup files.
-  this->backupModule.writeDishesBackUp(this->Dishes);
-  this->backupModule.writeDrinksBackUp(this->Drinks);
+  this->backupModule.writeRegistersBackUp(this->products);
   // Clears the vector memory.
-  this->Dishes.clear();
-  this->Drinks.clear();
+  this->products.clear();
   this->started = false;
 }
--- POS_Application/src/model/posmodel.h
+++ POS_Application/src/model/posmodel.h
@@ -1,6 +1,7 @@
 #ifndef POSMODEL_H
 #define POSMODEL_H
 
+#include <QString>
 #include <vector>
 #include <map>
 #include <string>
@@ -8,51 +9,150 @@
 #include "product.h"
 
 class POS_Model {
-private:
-  BackupModule& backupModule;
-  std::map<std::string, std::vector<Product>> Dishes;
-  std::map<std::string, std::vector<Product>> Drinks;
-  std::vector<SupplyItem> itemsOnInventory;
-  bool started = false;
-  
-  // Private class function.
-private:
-  POS_Model(BackupModule& module);
-  
-  // Public class function.
 public:
+  // Macros representing different types of register operations.
+  static const size_t UPDATE; ///< Represents an update operation.
+  static const size_t INSERT; ///< Represents an insert operation.
+  static const size_t DELETE; ///< Represents a delete operation.
+  
+private:
+  BackupModule& backupModule; ///< Reference to the BackupModule instance used for data persistence.
+  std::map<std::string, std::vector<Product>> products; ///< Map of dish categories to their respective products.
+  std::vector<std::pair<std::string, Product>> productsVector;
+  std::vector<SupplyItem> itemsOnInventory; ///< Inventory of supply items.
+  bool started = false; ///< Flag indicating whether the POS model has started.
+  
+public: ///< Class functions.
+  /**
+   * @brief Returns the singleton instance of the POS_Model class.
+   * 
+   * This function ensures that only one instance of the POS_Model class exists
+   * and initializes the BackupModule with the necessary file paths.
+   * 
+   * @return A reference to the singleton POS_Model instance.
+   */
   static POS_Model& getInstance();
-  
+  /**
+   * @brief Starts the POS_Model, loading backup data into memory.
+   * 
+   * This function reads the backup data for drinks and dishes, storing them 
+   * in the program's memory for later use during execution. It also sets the
+   * 'started' flag to true.
+   */
   void start();
-  
+  /**
+   * @brief Shuts down the POS_Model, saving data and cleaning up.
+   * 
+   * This function writes the current product data for drinks and dishes to the 
+   * backup files and clears the in-memory data.
+   */
   void shutdown();
   
-  std::vector<std::pair<std::string, Product>> registeredProducts();
-  
+  Product& findProduct(const std::string& productName);
+  /**
+   * @brief Updates the product register based on the specified operation.
+   * 
+   * This function modifies the product register based on the operation type 
+   * (INSERT or DELETE). It updates the backup files immediately after the 
+   * changes are made.
+   * 
+   * @param productCategory The category of the product to be added or removed.
+   * @param product The Product object to be added or removed.
+   * @param operation The type of operation (INSERT, UPDATE, DELETE) to be performed.
+   */
   void updateProductRegisters(const std::string productCategory,
-                              const Product& product);
+      const Product& product, const size_t operation);
   
-  // CLass getters.
-public:
-  inline bool isStarted() {
-    return this->started;  
-  }
+  void addProduct(const std::string& category, const Product& product);
   
-  inline std::map<std::string, std::vector<Product>> getRegisteredDrinks() {
-    return this->Drinks;
-  }
+  void removeProduct(const std::string& category, const Product& product);
   
-  inline std::map<std::string, std::vector<Product>> getRegisteredDishes() {
-    return this->Dishes;
-  }
+  void editProduct(const std::string& oldCategory, const Product& oldProduct,
+      const std::string& newCategory, const Product& newProduct);
+    
+  QString formatProductIngredients(
+      const std::vector<SupplyItem>& ingredients);
   
-private:
+private: ///< Class functions.
+  /**
+   * @brief Constructor for the POS_Model class.
+   * 
+   * Initializes the POS_Model instance with a reference to a BackupModule object.
+   * 
+   * @param module The BackupModule instance to interact with backup files.
+   */
+  POS_Model(BackupModule& module);
+  
+  /**
+   * @brief Extracts products from a product register and adds them to a vector.
+   * 
+   * This function transverses through a product register (a map of product categories) 
+   * and extracts all the products, adding them to the provided vector.
+   * 
+   * @param registeredProducts A vector to store the extracted products.
+   * @param productTypeRegister The map of registered products by category.
+   */
   void extractProducts(
       std::vector<std::pair<std::string, Product>>& registeredProducts
       , const std::map<std::string, std::vector<Product>>& productTypeRegister);
+  
+  /**
+   * @brief Inserts a product into the specified product register.
+   * 
+   * This function inserts a product into the product register if the product 
+   * doesn't already exist in the specified category. It checks for duplicates 
+   * and ensures that no identical product exists before adding it to the category.
+   * 
+   * @param productCategory The category of the product to be inserted.
+   * @param product The Product object to be inserted.
+   * @param productTypeRegister The register (map) of products.
+   * @return True if the product was inserted successfully, false if the product 
+   *         already exists in the category.
+   */
   bool insertOnRegister(const std::string productCategory
-      , const Product& product
-      , std::map<std::string, std::vector<Product>>& productTypeRegister);
+                        , const Product& product
+                        , std::map<std::string, std::vector<Product>>& productTypeRegister);
+  
+  /**
+   * @brief Removes a product from the specified product register.
+   * 
+   * This function removes a product from the product register, given the product's 
+   * category and the product itself. It searches through the product category and 
+   * deletes the product if found.
+   * 
+   * @param productCategory The category of the product to be removed.
+   * @param product The Product object to be removed.
+   * @param productTypeRegister The register (map) of products.
+   * @return True if the product was removed successfully, false if the product 
+   *         was not found in the category.
+   */
+  bool eraseOnRegister(const std::string productCategory
+                       , const Product& product
+                       , std::map<std::string, std::vector<Product>>& productTypeRegister);
+  
+public: ///< Class getters.
+  inline bool isStarted() {
+    return this->started;
+  }
+  
+  std::map<std::string, std::vector<Product>>& getRegisteredProducts() {
+    return this->products;
+  }
+  
+  size_t getNumberOfProducts() {
+    return this->productsVector.size();
+  }
+  
+  size_t getNumberOfCategories() {
+    return this->products.size();
+  }
+  
+  std::vector<std::string> getCategoriesForPage(size_t pageIndex);
+  
+  size_t getSizeOfCategory(std::string category);
+  
+  std::vector<std::pair<std::string, Product>> getProductsForPage(
+      size_t pageIndex);
 };
 
 #endif // APPMODEL_H
