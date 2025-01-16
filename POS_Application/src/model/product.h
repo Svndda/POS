b4bfc93bd59664236add5b9c92fcc1f61ffac6fd
Aaron Sanchez <aaroncz032@gmail.com>
2025-01-16 07:54:09 -0600

    doc: Doxygen documentation added.

--- POS_Application/src/model/product.h
+++ POS_Application/src/model/product.h
@@ -6,16 +6,36 @@
 
 #include "supplyitem.h"
 
+/**
+ * @class Product
+ * @brief Represents a product, which can be a food item or a drink.
+ * 
+ * The `Product` class encapsulates the attributes of a product, including 
+ * its unique ID, name, list of ingredients, and price. It provides getter and 
+ * setter methods for these attributes, as well as overloaded operators for 
+ * comparison and assignment.
+ */
 class Product {
   // Class Attributes.
 private:
-  uint64_t id = 0;
-  std::string name = "";
-  std::vector<SupplyItem> ingredients;
-  double price = 0;
+  uint64_t id = 0; ///< Unique identifier for the product.
+  std::string name = ""; ///< Name of the product.
+  std::vector<SupplyItem> ingredients; ///< List of ingredients for the product.
+  double price = 0; ///< Price of the product.
 
   // Class constructor.
 public:
+  /**
+   * @brief Constructs a new Product object.
+   * 
+   * Initializes the product with a given ID, name, list of ingredients, and price. 
+   * Default values are provided for each attribute if not specified.
+   * 
+   * @param myID The unique identifier for the product (default is 0).
+   * @param myName The name of the product (default is an empty string).
+   * @param myIngredients The list of ingredients for the product (default is an empty vector).
+   * @param myPrice The price of the product (default is 0).
+   */
   Product(uint64_t myID = 0
           , const std::string &myName = ""
           , const std::vector<SupplyItem> myIngredients
@@ -29,33 +49,84 @@ public:
 
   // Class Getters.
 public:
+  /**
+   * @brief Gets the unique identifier of the product.
+   * 
+   * @return The product's ID.
+   */
   inline const uint64_t getID() const {return this->id;}
   
+  /**
+   * @brief Gets the name of the product.
+   * 
+   * @return The product's name.
+   */
   inline const std::string getName() const {return this->name;}
   
+  /**
+   * @brief Gets the list of ingredients for the product.
+   * 
+   * @return A constant reference to the vector of ingredients.
+   */
   inline const std::vector<SupplyItem>& getIngredients() const {
     return this->ingredients;
   }
   
+  /**
+   * @brief Gets the price of the product.
+   * 
+   * @return The product's price.
+   */
   inline const double getPrice() const {return this->price;}
   
   // Class Setters.
 public:
+  /**
+   * @brief Sets the name of the product.
+   * 
+   * @param newName The new name of the product.
+   */
   inline void setName(const std::string &newName) {
     this->name = newName;
   }
   
+  /**
+   * @brief Sets the ingredients for the product.
+   * 
+   * @param newIngredients The new list of ingredients.
+   */
   inline void setIngredients(const std::vector<SupplyItem>
                                  &newIngredients) {
     this->ingredients = newIngredients;
   }
   
+  /**
+   * @brief Sets the price of the product.
+   * 
+   * @param newPrice The new price of the product.
+   */
   inline void setPrice(const double newPrice) {this->price = newPrice;}
   
 // Class Operators.
 public:
+  /**
+   * @brief Overloads the output stream operator to display the product details.
+   * 
+   * @param os The output stream object.
+   * @param product The product object to be output.
+   * @return The output stream with the product details.
+   */
   friend std::ostream& operator<<(std::ostream& os, const Product& product);
   
+  /**
+   * @brief Overloads the assignment operator to copy the values of another product.
+   * 
+   * This operator ensures that all attributes of the product are copied.
+   * It prevents self-assignment.
+   * 
+   * @param other The other product to be copied.
+   * @return A reference to the current product object.
+   */
   Product &operator=(const Product &other) {
     if (this == &other) {
       // Evitar autoasignación
@@ -71,13 +142,23 @@ public:
     return *this; // Devolver la referencia al objeto actual
   }
   
+  /**
+   * @brief Overloads the equality operator to compare two products.
+   * 
+   * Compares the products based on their ID, name, price, and ingredients.
+   * 
+   * @param other The other product to compare.
+   * @return True if the products are equal, false otherwise.
+   */
   bool operator==(const Product &other) const {
     return this->id == other.id &&
-           this->name == other.name &&
-           this->ingredients == other.ingredients &&
-           this->price == other.price;
+          this->name == other.name &&
+          this->price == other.price &&
+          std::equal(this->ingredients.begin(),
+                    this->ingredients.end(),
+                    other.ingredients.begin(),
+                    other.ingredients.end());
   }
-  
 };
 
 #endif // PRODUCT_H
--- POS_Application/src/model/supplyitem.h
+++ POS_Application/src/model/supplyitem.h
@@ -4,32 +4,79 @@
 #include <cstdint>
 #include <string>
 
+/**
+ * @class SupplyItem
+ * @brief Represents an item in the inventory used for products.
+ * 
+ * The `SupplyItem` class encapsulates the attributes of an inventory item, 
+ * including its name and quantity. It provides getter and setter methods 
+ * for these attributes, and an overloaded equality operator for comparison.
+ */
 class SupplyItem {
 
 // Class Attributes.
 private:
-  std::string name = "";
-  uint64_t quantity = 0;
+  std::string name = ""; ///< Name of the supply item.
+  uint64_t quantity = 0; ///< Quantity of the supply item in inventory.
 
 // Class Constructor.
 public:
+  /**
+   * @brief Constructs a new SupplyItem object.
+   * 
+   * Initializes the supply item with a given name and quantity. Default values 
+   * are provided if not specified.
+   * 
+   * @param myName The name of the supply item (default is an empty string).
+   * @param myQuantity The quantity of the supply item (default is 0).
+   */
   SupplyItem(const std::string& myName = "", uint64_t myQuantity = 0)
     : name(myName),
       quantity(myQuantity) {
   };
   
+  /**
+   * @brief Overloads the equality operator to compare two supply items.
+   * 
+   * Compares the supply items based on their name and quantity.
+   * 
+   * @param other The other supply item to compare.
+   * @return True if the supply items are equal, false otherwise.
+   */
   bool operator==(const SupplyItem& other) const {
     return this->name == other.name && this->quantity == other.quantity;
   }
 
 // Class Getters.
 public:
+  /**
+   * @brief Gets the name of the supply item.
+   * 
+   * @return A constant reference to the name of the supply item.
+   */
   inline const std::string& getName() const {return this->name;};
+  
+  /**
+   * @brief Gets the quantity of the supply item.
+   * 
+   * @return The quantity of the supply item.
+   */
   inline const uint64_t getQuantity() const {return this->quantity;}
 
 // Class Setters.
 public:
+  /**
+   * @brief Sets the name of the supply item.
+   * 
+   * @param newName The new name for the supply item.
+   */
   inline void setName(const std::string& newName) {this->name = newName;}
+  
+  /**
+   * @brief Sets the quantity of the supply item.
+   * 
+   * @param newQuantity The new quantity for the supply item.
+   */
   inline void setQuantity(const uint64_t newQuantity) {
     this->quantity = newQuantity;
   }
